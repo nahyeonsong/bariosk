ae766744a7f27f578de50d41ea4e6751e9b9f4cc
Nahyeon Song <sny0708@naver.com>
2025-05-06 16:16:06 +0900

    25.5.6/인영영

--- script.js
+++ script.js
@@ -1987,12 +1987,17 @@ async function handleCategoryDrop(e) {
         const categoryList = document.getElementById("categoryList");
         const categoryItems = categoryList.querySelectorAll(".category-item");
         const categories = [];
-
+        /*
         // 새 순서로 카테고리 목록 구성
         categoryItems.forEach((item) => {
             categories.push(item.dataset.category);
         });
-
+        */
+        [categories[categoryDragStartIndex], categories[categoryDragEndIndex]] =
+            [
+                categories[categoryDragStartIndex],
+                categories[categoryDragStartIndex],
+            ];
         console.log(`새 카테고리 순서:`, categories);
 
         // 로컬 스토리지에 새 순서 즉시 저장
